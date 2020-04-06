f65f3a865b60c90dbb36c6591554e5e1ec542ead
BlazorPlusD <account@blazorplus.com>
2020-04-06 14:17:16 +0800

    Add project files.

---
+++ BlazorDemoWhyBlazor/BSModeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using BlazorDemoWhyBlazor.Data;
+
+namespace BlazorDemoWhyBlazor
+{
+	public class BSModeController : Controller
+	{
+		public class MessageBoardViewState
+		{
+			public string InputUserName { get; set; }
+			public string InputTitle { get; set; }
+			public string InputMessage { get; set; }
+		}
+		public class MessageBoardViewModel
+		{
+			public List<MyMsg> Msgs
+			{
+				get
+				{
+					return MyMsg.Msgs;
+				}
+			}
+
+			public MessageBoardViewState State { get; set; } = new MessageBoardViewState();
+
+			public string PostType { get; set; }
+			public string PostResult { get; set; }
+			public string PostResultMessage { get; set; }
+		}
+
+		[HttpGet]
+		public IActionResult MessageBoard()
+		{
+			return View(new MessageBoardViewModel());
+		}
+
+		[HttpPost]
+		public IActionResult MessageBoard(string post_action, string _state)
+		{
+			string[] actionarr = post_action.Split(':');
+
+			MessageBoardViewModel vm = new MessageBoardViewModel();
+			if (!string.IsNullOrEmpty(_state))
+			{
+				vm.State = System.Text.Json.JsonSerializer.Deserialize<MessageBoardViewState>(_state);
+			}
+
+			vm.PostType = actionarr[0];
+			vm.State.InputUserName = Request.Form["inp_username"];
+			vm.State.InputTitle = Request.Form["inp_title"];
+			vm.State.InputMessage = Request.Form["inp_message"];
+
+			if (vm.PostType == "PostMsg")
+			{
+				if (string.IsNullOrWhiteSpace(vm.State.InputUserName))
+				{
+					vm.PostResult = "Failed";
+					vm.PostResultMessage = "Please type username";
+					return View(vm);
+				}
+				if (string.IsNullOrWhiteSpace(vm.State.InputTitle))
+				{
+					vm.PostResult = "Failed";
+					vm.PostResultMessage = "Please type title";
+					return View(vm);
+				}
+				if (string.IsNullOrWhiteSpace(vm.State.InputMessage))
+				{
+					vm.PostResult = "Failed";
+					vm.PostResultMessage = "Please type message";
+					return View(vm);
+				}
+
+				MyMsg msg = MyMsg.AddMsg(vm.State.InputUserName, vm.State.InputTitle, vm.State.InputMessage);
+				vm.PostResult = "Done";
+				vm.PostResultMessage = "Message #" + msg.MsgId + " have been added";
+				//Keep UserName //vm.State.InputUserName = "";
+				vm.State.InputTitle = "";//clear Title
+				vm.State.InputMessage = "";//clear Message
+				return View(vm);
+			}
+
+			if (vm.PostType == "Delete" || vm.PostType == "Comment")
+			{
+				int id = int.Parse(actionarr[1]);
+				var msg = MyMsg.Msgs.Where(v => v.MsgId == id).FirstOrDefault();
+				if (msg == null)
+				{
+					vm.PostResult = "Failed";
+					vm.PostResultMessage = "The message maybe deleted already";
+					return View(vm);
+				}
+				if (vm.PostType == "Delete")
+				{
+					MyMsg.Msgs.Remove(msg);
+					vm.PostResult = "Done";
+					vm.PostResultMessage = "Message #" + msg.MsgId + " have been deleted";
+					return View(vm);
+				}
+				if (vm.PostType == "Comment")
+				{
+					string comment = Request.Form["inp_comment_" + id];
+					if (string.IsNullOrWhiteSpace(comment))
+					{
+						vm.PostResult = "Failed";
+						vm.PostResultMessage = "Please type comment";
+						return View(vm);
+					}
+					msg.Comments.Add(comment);
+					vm.PostResult = "Done";
+					vm.PostResultMessage = "Comment have been added";
+					return View(vm);
+				}
+			}
+
+			vm.PostResult = "Failed";
+			vm.PostResultMessage = "Unknown action " + post_action;
+
+			return View(vm);
+		}
+
+	}
+
+}
---
+++ BlazorDemoWhyBlazor/Data/MyMsg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemoWhyBlazor.Data
+{
+	public class MyMsg
+	{
+		public int MsgId { get; set; }
+		public DateTime PostTime { get; set; }
+		public string UserName { get; set; }
+		public string Title { get; set; }
+		public string Message { get; set; }
+
+		public List<string> Comments { get; } = new List<string>();
+
+
+		static public List<MyMsg> Msgs = new List<MyMsg>();
+
+		static public int NextMsgId = 1;
+
+		static public MyMsg AddMsg(string userame,string title, string message)
+		{
+			MyMsg msg = new MyMsg();
+			msg.MsgId = NextMsgId++;
+			msg.PostTime = DateTime.Now;
+			msg.UserName = userame;
+			msg.Title = title;
+			msg.Message = message;
+			Msgs.Add(msg);
+			return msg;
+		}
+	}
+
+}
